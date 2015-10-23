6b18a0ab08179c08b9df1042a54bcd10d6d3d76c
Samuel <sbneto@users.noreply.github.com>
2015-10-23 18:48:58 -0400

    some jquery

--- javascript/script.js
+++ javascript/script.js
@@ -19,43 +19,13 @@ function load_publications() {
     $("#main").load("publications.html", function() {
         if ($(".bibtex_template").size() == 0) {
             $("body").append("<div class=\"bibtex_template\"><div class=\"if author\" style=\"font-weight: bold;\">\n  <span class=\"if year\">\n    <span class=\"year\"></span>, \n  </span>\n  <span class=\"author\"></span>\n  <span class=\"if url\" style=\"margin-left: 20px\">\n    <a class=\"url\" style=\"color:black; font-size:10px\">(view online)</a>\n  </span>\n</div>\n<div style=\"margin-left: 10px; margin-bottom:5px;\">\n  <span class=\"title\"></span>\n</div></div>");
+            $(".bibtex_template").hide();
         }
-        $(".bibtex_template").hide();
-        (new BibtexDisplay()).displayBibtex(disp, $("#bibtex_display"));
+        $.get("publications.bib", function(bibtex) {
+            (new BibtexDisplay()).displayBibtex(bibtex, $("#bibtex_display"));
+        });
     });}
 
 function load_work() {
     $("#main").load("work.html");
 }
-
-var disp = '  @MISC{antlr:targets,\
-        AUTHOR = "Terance Parr et al",\
-        TITLE = "ANTLR code generation targets",\
-        MONTH = "May",\
-        YEAR = {2010},\
-        NOTE = "\\url{http://www.antlr.org/wiki/display/ANTLR3/Code+Generation+Targets?focusedCommentId=23232603#comment-23232603}"\
-  }\
-\
-  @MISC{antlr:works,\
-        AUTHOR = "Jean Bovet and Terance Parr",\
-        TITLE = "ANTLRWorks: An ANTLR Grammar Development Environment",\
-        MONTH = "July",\
-        YEAR = {2007},\
-        NOTE = "\\url{http://www.antlr.org/papers/antlrworks-draft.pdf}"\
-  }\
-\
-  @MISC{antlr:keywords,\
-        AUTHOR = "Terance Parr",\
-        TITLE = "How can I allow keywords as identifiers?",\
-        MONTH = "June",\
-        YEAR = {2008},\
-        NOTE = "\\url{http://www.antlr.org/wiki/pages/viewpage.action?pageId=1741}"\
-  }\
-\
-  @misc{ wiki:ast,\
-    author = "Wikipedia",\
-    title = "Abstract syntax tree --- Wikipedia{,} The Free Encyclopedia",\
-    year = "2010",\
-    url = "\\url{http://en.wikipedia.org/w/index.php?title=Abstract_syntax_tree&oldid=351440895}",\
-    note = "[Online; accessed 30-March-2010]"\
-  }'
