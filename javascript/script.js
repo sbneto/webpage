556927b47aa4cede67ef5d1f0047de0b069d861f
Samuel <sbneto@users.noreply.github.com>
2015-10-23 15:21:29 -0400

    some jquery

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     load_header();
     load_home();
+    $("home").onclick = load_home
+    $("about").onclick = load_about
+    $("publications").onclick = load_publications
+    $("work").onclick = load_work
 });
 
 function load_header() {
@@ -9,4 +13,16 @@ function load_header() {
 
 function load_home() {
     $("#main").load('home.html');
+}
+
+function load_about() {
+    $("#main").load('about.html');
+}
+
+function load_publications() {
+    $("#main").load('publications.html');
+}
+
+function load_work() {
+    $("#main").load('work.html');
 }
