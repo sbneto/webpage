1bacbe5453338ce002ea393ad663ac635db66bfc
Samuel <sbneto@users.noreply.github.com>
2015-10-23 19:37:05 -0400

    some jquery

--- javascript/script.js
+++ javascript/script.js
@@ -22,7 +22,7 @@ function load_publications() {
     deferred.push($.get("publications.html", function(data) {$("#main").html(data);}));
     deferred.push($.get("publications.bib", function(data, bibtex) {bibtex = data;}));
     deferred.push($.get("bibtex_template.html", function(data, template) {template = data;}));
-    $.when(deferred).done(function() {
+    $.when.apply($, deferred).done(function() {
         (new BibtexDisplay()).displayBibtex(bibtex, $("#bibtex_display"), template)
     });
     // $("#main").load("publications.html", function() {
