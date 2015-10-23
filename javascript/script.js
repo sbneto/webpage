ab3888d2924f83567791adffc31227f1315fe467
Samuel <sbneto@users.noreply.github.com>
2015-10-23 18:22:14 -0400

    some jquery

--- javascript/script.js
+++ javascript/script.js
@@ -26,34 +26,34 @@ function load_work() {
     $("#main").load("work.html");
 }
 
-var disp = '  @MISC{antlr:targets,
-        AUTHOR = "Terance Parr et al",
-        TITLE = "ANTLR code generation targets",
-        MONTH = "May",
-        YEAR = {2010},
-        NOTE = "\\url{http://www.antlr.org/wiki/display/ANTLR3/Code+Generation+Targets?focusedCommentId=23232603#comment-23232603}"
-  }
-
-  @MISC{antlr:works,
-        AUTHOR = "Jean Bovet and Terance Parr",
-        TITLE = "ANTLRWorks: An ANTLR Grammar Development Environment",
-        MONTH = "July",
-        YEAR = {2007},
-        NOTE = "\\url{http://www.antlr.org/papers/antlrworks-draft.pdf}"
-  }
-
-  @MISC{antlr:keywords,
-        AUTHOR = "Terance Parr",
-        TITLE = "How can I allow keywords as identifiers?",
-        MONTH = "June",
-        YEAR = {2008},
-        NOTE = "\\url{http://www.antlr.org/wiki/pages/viewpage.action?pageId=1741}"
-  }
-
-  @misc{ wiki:ast,
-    author = "Wikipedia",
-    title = "Abstract syntax tree --- Wikipedia{,} The Free Encyclopedia",
-    year = "2010",
-    url = "\\url{http://en.wikipedia.org/w/index.php?title=Abstract_syntax_tree&oldid=351440895}",
-    note = "[Online; accessed 30-March-2010]"
+var disp = '  @MISC{antlr:targets,\
+        AUTHOR = "Terance Parr et al",\
+        TITLE = "ANTLR code generation targets",\
+        MONTH = "May",\
+        YEAR = {2010},\
+        NOTE = "\\url{http://www.antlr.org/wiki/display/ANTLR3/Code+Generation+Targets?focusedCommentId=23232603#comment-23232603}"\
+  }\
+\
+  @MISC{antlr:works,\
+        AUTHOR = "Jean Bovet and Terance Parr",\
+        TITLE = "ANTLRWorks: An ANTLR Grammar Development Environment",\
+        MONTH = "July",\
+        YEAR = {2007},\
+        NOTE = "\\url{http://www.antlr.org/papers/antlrworks-draft.pdf}"\
+  }\
+\
+  @MISC{antlr:keywords,\
+        AUTHOR = "Terance Parr",\
+        TITLE = "How can I allow keywords as identifiers?",\
+        MONTH = "June",\
+        YEAR = {2008},\
+        NOTE = "\\url{http://www.antlr.org/wiki/pages/viewpage.action?pageId=1741}"\
+  }\
+\
+  @misc{ wiki:ast,\
+    author = "Wikipedia",\
+    title = "Abstract syntax tree --- Wikipedia{,} The Free Encyclopedia",\
+    year = "2010",\
+    url = "\\url{http://en.wikipedia.org/w/index.php?title=Abstract_syntax_tree&oldid=351440895}",\
+    note = "[Online; accessed 30-March-2010]"\
   }'
