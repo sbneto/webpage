dcb9d1c9d75f7d155a3c9e49bf502ced314ce6b2
Samuel <sbneto@users.noreply.github.com>
2015-10-23 15:36:58 -0400

    some jquery

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,28 @@
 $(document).ready(function () {
     load_header();
     load_home();
-    $("#home").click(load_home)
-    $("#about").click(load_about)
-    $("#publications").click(load_publications)
-    $("#work").click(load_work)
+    document.getElementById("home").onclick = load_home
+    document.getElementById(("about").onclick = load_about
+    document.getElementById(("publications").onclick = load_publications
+    document.getElementById(("work").onclick = load_work
 });
 
 function load_header() {
-    event.preventDefault();
     $("#header").load('header.html');
 }
 
 function load_home() {
-    event.preventDefault();
     $("#main").load('home.html');
 }
 
 function load_about() {
-    event.preventDefault();
     $("#main").load('about.html');
 }
 
 function load_publications() {
-    event.preventDefault();
     $("#main").load('publications.html');
 }
 
 function load_work() {
-    event.preventDefault();
     $("#main").load('work.html');
 }
